e2fc4a99af59a039aa59008441f5cae2f1524876
wogle <wogle@localhost>
2024-10-02 15:19:43 -0500

    Completed

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,25 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+	double[] x = {0.3,0.3,0.5};
+	double[] y = {0.6,0.4,0.5};
+		
+	StdDraw.setPenColor(103,221,255);
+	StdDraw.filledRectangle(0.5, 0.5, 2.5/6, 1.5/6);
+	StdDraw.setPenColor(250,128,114);
+	StdDraw.filledEllipse(0.5, 0.5, 0.15, 0.085);
+	StdDraw.filledPolygon(x, y);
+	StdDraw.setPenColor(0,0,0);
+	StdDraw.filledCircle(0.61, 0.52, .01);
+	StdDraw.arc(0.63, 0.52, 0.05, 250, 282);
+	StdDraw.setPenColor(255,255,255);
+	StdDraw.circle(0.2, 0.4, 0.1);
+	StdDraw.circle(0.3, 0.7, 0.1);
+	StdDraw.circle(0.7, 0.3, 0.1);
+	StdDraw.circle(0.6, 0.7, 0.1);
+	StdDraw.circle(0.8, 0.4, 0.1);
+	StdDraw.setPenColor(0,0,0);
+	StdDraw.rectangle(0.5, 0.5, 2.5/6, 1.5/6);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -12,13 +12,61 @@ import edu.princeton.cs.introcs.StdDraw;
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
-public class InterpretDrawingFile {
+public class InterpretDrawingFile {//ellipse 73 0 146 false 0.5 0.5 0.2 0.2
 
 	public static void main(String[] args) throws FileNotFoundException {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = "";
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		boolean filled = true;
+		double x = 0;
+		double y = 0;
+		double halfwidth = 0;
+		double halfheight = 0;
+		
+		while(in.hasNext())
+		{
+	
+			if(in.hasNextInt())
+			{
+				r = in.nextInt();
+				g = in.nextInt();
+				b = in.nextInt();
+			}
+			if(in.hasNextBoolean())
+			{
+				filled = in.nextBoolean();
+			}
+			if(in.hasNextDouble())
+			{
+				x = in.nextDouble();
+				y = in.nextDouble();
+				halfwidth = in.nextDouble();
+				halfheight = in.nextDouble();
+			}
+			else
+			{
+				shape = in.next();
+			}
+		}
+		
+		StdDraw.setPenColor(r,g,b);
+		
+		if(shape.equals("ellipse"))
+		{
+			if(filled)
+			{	
+			StdDraw.filledEllipse(x, y, halfwidth, halfheight);
+			}
+			else
+			StdDraw.ellipse(x, y, halfwidth, halfheight);
+		}
 		
 	}
 }
